41b0d395bbbe20adb53c39e97fa9c52dcc398d90
Isaev Anton <Isaevantony220@yandex.ru>
2021-06-24 15:23:51 +0300

    Добавлен тест создания нового пользователя newUser

--- src/main/java/steps/Steps.java
+++ src/main/java/steps/Steps.java
@@ -1,6 +1,8 @@
 package steps;
 
 import data.classes.Data;
+import data.classes.People;
+import data.classes.PeopleCreated;
 import data.classes.Resource;
 import io.qameta.allure.Step;
 import org.testng.Assert;
@@ -10,19 +12,19 @@ import static io.restassured.RestAssured.given;
 public class Steps {
     @Step("Проверяем Json Resource на отсутствие нулевых полей")
     public static void checkNotNull(Resource resource){
-        Assert.assertNotNull(resource.getPage(), "Поле колличество страниц пустое");
-        Assert.assertNotNull(resource.getPer_page(), "Поле колличество страниц пустое");
-        Assert.assertNotNull(resource.getTotal(), "Поле колличество страниц пустое");
+        Assert.assertNotNull(resource.getPage(), "Поле номер страницы пустое");
+        Assert.assertNotNull(resource.getPer_page(), "Поле колличество объектов на странице пустое");
+        Assert.assertNotNull(resource.getTotal(), "Поле Total пустое");
         Assert.assertNotNull(resource.getTotal_pages(), "Поле колличество страниц пустое");
         for (Data data : resource.getData()){
-            Assert.assertNotNull(data.getLast_name());
-            Assert.assertNotNull(data.getAvatar());
-            Assert.assertNotNull(data.getEmail());
-            Assert.assertNotNull(data.getId());
-            Assert.assertNotNull(data.getFirst_name());
+            Assert.assertNotNull(data.getLast_name(), "Поле фамилия пустое");
+            Assert.assertNotNull(data.getAvatar(),"Поле аватар пустое");
+            Assert.assertNotNull(data.getEmail(),"Поле email пустое");
+            Assert.assertNotNull(data.getId(),"Поле id пустое");
+            Assert.assertNotNull(data.getFirst_name(),"Поле имя пустое");
         }
-        Assert.assertNotNull(resource.getSupport().getUrl(), "Поле колличество страниц пустое");
-        Assert.assertNotNull(resource.getSupport().getText(), "Поле колличество страниц пустое");
+        Assert.assertNotNull(resource.getSupport().getUrl(), "Поле URL пустое");
+        Assert.assertNotNull(resource.getSupport().getText(), "Поле text пустое");
     }
     @Step("Получаем данные со страницы")
     public static Resource getUserDataFromPage(){
@@ -33,5 +35,26 @@ public class Steps {
                 .log().body()
                 .extract().body().as(Resource.class);
         return resource;
+    }
+
+    @Step("Проверяем дынные при создании нового пользователя")
+    public static void checkNewUser(People people, PeopleCreated peopleCreated){
+        Assert.assertEquals(people.getJob(), peopleCreated.getJob());
+        Assert.assertEquals(people.getName(), peopleCreated.getName());
+    }
+
+    @Step("Создаем нового пользователя")
+    public static PeopleCreated getDataNewUser(){
+        People people = new People("morpheus", "leader");
+        PeopleCreated peopleCreated = given()
+                .contentType("application/json")
+                .body(people)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract().as(PeopleCreated.class);
+        return peopleCreated;
     }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,32 +1,23 @@
 import data.classes.People;
 import data.classes.PeopleCreated;
 import data.classes.Resource;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static steps.Steps.checkNotNull;
-import static steps.Steps.getUserDataFromPage;
+import static steps.Steps.*;
 
 public class Tests {
     @Test
-    public void test_1() {
+    public void getUsers() {
         Resource resource = getUserDataFromPage();
         checkNotNull(resource);
 
     }
-
-    @Test()
-    public void dtoTest(){
+    @Test
+    public void newUser(){
         People people = new People("morpheus", "leader");
-        PeopleCreated peopleCreated = given()
-                .contentType("application/json")
-                .body(people)
-                .when()
-                .post("https://reqres.in/api/users")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .extract().as(PeopleCreated.class);
-        System.out.println(peopleCreated.getCreatedAt());
+        PeopleCreated peopleCreated = getDataNewUser();
+        checkNewUser(people, peopleCreated);
     }
 }
